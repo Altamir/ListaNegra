dd131e08fa38a84b8c2f836700482e28236496d5
altamir.benkenstein@outlook.com <altamir.benkenstein@outlook.com>
2015-10-27 15:49:03 +0000

    troca de micro

---
+++ app/Http/Requests/RotuloRequestCreate.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ListaNegra\Http\Requests;
+
+use ListaNegra\Http\Requests\Request;
+
+class RotuloRequestCreate extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+           'name' => 'required|min:7|max:7',
+           'cor'  => 'require|min:'
+        ];
+    }
+}
